46d0eee6c73294ad5396b1b12c613a8024c9c255
kukoven <kukoven@gmail.com>
2021-07-10 18:24:37 +0300

    Добавил react-компоненты и валидацию props

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page.jsx';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({promoFilm, films}) {
+  return (
+    <MainPage promoFilm={promoFilm} films={films}/>
+  );
 }
+
+App.propTypes = {
+  promoFilm: PropTypes.object.isRequired,
+  films: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default App;
---
+++ project/src/mock.js
@@ -0,0 +1,115 @@
+const FILMS = [
+  {
+    id: 0,
+    title: 'Fantastic Beasts: The Crimes of Grindelwald',
+    poster: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  },
+  {
+    id: 1,
+    title: 'Bohemian Rhapsody',
+    poster: 'img/bohemian-rhapsody.jpg',
+  },
+  {
+    id: 2,
+    title: 'Macbeth',
+    poster: 'img/macbeth.jpg',
+  },
+  {
+    id: 3,
+    title: 'Aviator',
+    poster: 'img/aviator.jpg',
+  },
+  {
+    id: 4,
+    title: 'We need to talk about Kevin',
+    poster: 'img/we-need-to-talk-about-kevin.jpg',
+  },
+  {
+    id: 5,
+    title: 'What We Do in the Shadows',
+    poster: 'img/what-we-do-in-the-shadows.jpg',
+  },
+  {
+    id: 6,
+    title: 'Revenant',
+    poster: 'img/revenant.jpg',
+  },
+  {
+    id: 7,
+    title: 'Johnny English',
+    poster: 'img/johnny-english.jpg',
+  },
+  {
+    id: 8,
+    title: 'Shutter Island',
+    poster: 'img/shutter-island.jpg',
+  },
+  {
+    id: 9,
+    title: 'Pulp Fiction',
+    poster: 'img/pulp-fiction.jpg',
+  },
+  {
+    id: 10,
+    title: 'No Country for Old Men',
+    poster: 'img/no-country-for-old-men.jpg',
+  },
+  {
+    id: 11,
+    title: 'Snatch',
+    poster: 'img/snatch.jpg',
+  },
+  {
+    id: 12,
+    title: 'Moonrise Kingdom',
+    poster: 'img/moonrise-kingdom.jpg',
+  },
+  {
+    id: 13,
+    title: 'Seven Years in Tibet',
+    poster: 'img/seven-years-in-tibet.jpg',
+  },
+  {
+    id: 14,
+    title: 'Midnight Special',
+    poster: 'img/midnight-special.jpg',
+  },
+  {
+    id: 15,
+    title: 'War of the Worlds',
+    poster: 'img/war-of-the-worlds.jpg',
+  },
+  {
+    id: 16,
+    title: 'Dardjeeling Limited',
+    poster: 'img/dardjeeling-limited.jpg',
+  },
+  {
+    id: 17,
+    title: 'Orlando',
+    poster: 'img/orlando.jpg',
+  },
+  {
+    id: 18,
+    title: 'Mindhunter',
+    poster: 'img/mindhunter.jpg',
+  },
+  {
+    id: 19,
+    title: 'Midnight Special',
+    poster: 'img/midnight-special.jpg',
+  },
+];
+
+const PROMO_FILM = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+  poster: 'img/the-grand-budapest-hotel-poster.jpg',
+  bigPoster: 'img/bg-the-grand-budapest-hotel.jpg',
+};
+
+export {
+  FILMS,
+  PROMO_FILM
+};
